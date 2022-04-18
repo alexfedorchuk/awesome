f7db7ca24c3f93c60d191c502ee77e9d908f86a9
oleksii.fedorchuk <alexfedorchukw@gmail.com>
2022-04-18 13:32:11 +0300

    feat: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,8 +24,8 @@ export default class MyDocument extends Document<MyDocumentProps> {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> */}
+          {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
