924f712a7120ae06136e65eb0366cf8d55d420a0
oleksii.fedorchuk <alexfedorchukw@gmail.com>
2022-05-09 12:57:34 +0300

    feat: fixed navigation bottom

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 
 import Link from 'Components/link';
 import Page from 'Components/page';
+import Container from '@mui/material/Container';
+import NavigationBottom from 'Components/common/navigation-bottom';
 
 import Routes from 'Config/routes';
 
@@ -12,12 +14,50 @@ const Home: NextPage = () => (
     <Head>
       <title>Welcome to Awesome!</title>
     </Head>
-    <p>
-      Content
-      <Link href={Routes.About}>
-        Go to about
-      </Link>
-    </p>
+    <Container>
+      <p>
+        Content
+        <Link href={Routes.About}>
+          Go to about
+        </Link>
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, hic iusto esse optio ducimus cum minus dolorum expedita odio! Ipsum ut velit inventore esse iste nemo cupiditate, aliquid doloribus deleniti!
+      </p>
+    </Container>
+    <NavigationBottom />
   </Page>
 );
 
---
+++ src/components/common/navigation-bottom.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+
+const NavigationBottom: React.FC = () => (
+  <Box sx={{ position: 'fixed', bottom: 0, bottom: 'env(safe-area-inset-bottom)', width: '100%', backgroundColor: '#fff', py: 2, borderTop: 1 }}>
+    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div>
+        1
+      </div>
+      <div>
+        2
+      </div>
+      <div>
+        3
+      </div>
+      <div>
+        4
+      </div>
+    </Box>
+  </Box>
+);
+
+export default NavigationBottom;
